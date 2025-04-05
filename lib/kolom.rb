1acee79594db6844ef712c8b85adddbb057521f2
smakthe <scmakra99@gmail.com>
2025-04-05 08:48:54 +0530

    First commit: Initialized project

---
+++ lib/kolom.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kolom/version"
+
+module Kolom
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kolom/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Kolom
+  VERSION = "0.1.0"
+end
